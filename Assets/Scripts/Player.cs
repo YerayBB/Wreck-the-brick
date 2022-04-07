d0e8b6d31972d2956eb27c2cc2fbd954244f0229
YerayBB <yeraybosoms@gmail.com>
2022-04-08 00:55:13 +0200

    player early set up for powerups + able to attach ball

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UtilsUnknown.Extensions;
+using UnityEngine.InputSystem;
 
 namespace WreckTheBrick
 {
@@ -12,30 +15,50 @@ namespace WreckTheBrick
         [SerializeField]
         private float _maxBounceAngle = 75f;
 
+        private Transform _transform;
         private Rigidbody2D _rigidbody;
         private Controls _inputs;
 
         private bool _leftMove = false;
         private bool _rightMove = false;
+        private uint _stickiness = 0;
+
+        private Ball _attachedBall = null;
+        private Coroutine _releaseBallTimer;
+
+
 
 
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _transform = transform;
+
             _inputs = new Controls();
             _inputs.Player.Left.performed += (context) => _leftMove = true;
             _inputs.Player.Left.canceled += (context) => _leftMove = false;
             _inputs.Player.Right.performed += (context) => _rightMove = true;
             _inputs.Player.Right.canceled += (context) => _rightMove = false;
+            _inputs.Player.Shot.performed += (context) => ReleaseBall();
             _inputs.Player.Enable();
 
+        }
+
+        private void ReleaseBall()
+        {
+            if(_attachedBall != null)
+            {
+                if (_releaseBallTimer != null) StopCoroutine(_releaseBallTimer);
+                _attachedBall.Release(_transform.up);
+                _attachedBall = null;
+            }
         }
 
         // Start is called before the first frame update
         void Start()
         {
-
+            AttachBall(GameManager.Instance.SpawnBall(_transform.position + Vector3.up));
         }
 
         // Update is called once per frame
@@ -61,18 +84,32 @@ namespace WreckTheBrick
             Ball ball;
             if(collision.gameObject.TryGetComponent<Ball>(out ball))
             {
-                float offset =  transform.position.x - collision.GetContact(0).point.x;
-                float width = collision.otherCollider.bounds.size.x / 2;
+                if (_stickiness == 0)
+                {
+                    float offset = _transform.position.x - collision.GetContact(0).point.x;
+                    float width = collision.otherCollider.bounds.size.x / 2;
 
-                float entranceAngle = Vector2.SignedAngle(transform.up, ball.GetDirection());
-                Debug.Log($"angle: {entranceAngle} direction: {ball.GetDirection()}");
-                float bounceAngle = offset / width * _maxBounceAngle;
-                bounceAngle = Mathf.Clamp(bounceAngle + entranceAngle, -_maxBounceAngle, _maxBounceAngle);
+                    float entranceAngle = Vector2.SignedAngle(_transform.up, ball.GetDirection());
+                    float bounceAngle = offset / width * _maxBounceAngle;
+                    bounceAngle = Mathf.Clamp(bounceAngle + entranceAngle, -_maxBounceAngle, _maxBounceAngle);
 
-                Quaternion rotation = Quaternion.AngleAxis(bounceAngle, Vector3.forward);
+                    Quaternion rotation = Quaternion.AngleAxis(bounceAngle, Vector3.forward);
 
-                ball.SetDirection(rotation * transform.up);
+                    ball.SetDirection(rotation * _transform.up);
+                }
+                else
+                {
+                    AttachBall(ball);
+                    _releaseBallTimer = this.DelayedCall(ReleaseBall, _stickiness * 5);
+                }
             }
+        }
+
+        public void AttachBall(Ball ball)
+        {
+            if (_attachedBall != null) ReleaseBall();
+            _attachedBall = ball;
+            _attachedBall.Stick(_transform);
         }
     }
 }
