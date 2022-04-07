9b3f50d48099c98f582aaca29a707a4c118a478a
YerayBB <yeraybosoms@gmail.com>
2022-04-08 00:53:14 +0200

    created powerup

---
+++ Assets/Scripts/PowerUpData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WreckTheBrick
+{
+    [CreateAssetMenu(fileName = "new PowerUp", menuName = "Scriptable/Power Up")]
+    public class PowerUpData : ScriptableObject
+    {
+
+
+    }
+}
