b00bf0de5b0885f4c150b549406b0df24fa8e769
YerayBB <yeraybosoms@gmail.com>
2022-04-09 18:34:50 +0200

    brick ready to pool

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -5,23 +5,52 @@ using UtilsUnknown;
 
 namespace WreckTheBrick
 {
+    [RequireComponent(typeof(BoxCollider2D), typeof(SpriteRenderer))]
     public class Brick : PoolableBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        private SpriteRenderer _renderer;
+        private Transform _transform;
 
+        private bool _inmmune = false;
+        private int _hp = 0;
+
+        public System.Action<int> OnHpLost;
+
+
+        private void Awake()
+        {
+            _transform = transform;
+            _renderer = GetComponent<SpriteRenderer>();
         }
 
-        // Update is called once per frame
-        void Update()
+        public void Initialize(Vector3 pos, Vector3 size, BrickData data)
         {
-
+            _init = false;
+            _transform.position = pos;
+            _transform.localScale = size;
+            _inmmune = data.inmune;
+            _hp = data.maxHP;
+            _renderer.sprite = data.sprite;
+            gameObject.SetActive(true);
+            _init = true;
         }
 
-        public void SetData(BrickData data)
+        private void OnCollisionEnter2D(Collision2D collision)
         {
-
+            if (_init)
+            {
+                if (!_inmmune)
+                {
+                    Ball ball;
+                    if (collision.gameObject.TryGetComponent<Ball>(out ball))
+                    {
+                        int amount = Mathf.Min((int) ball.damage, _hp);
+                        _hp -= amount;
+                        OnHpLost?.Invoke(amount);
+                        if (_hp <= 0) Disable();
+                    }
+                }
+            }
         }
     }
 }
--- Assets/Scripts/BrickData.cs
+++ Assets/Scripts/BrickData.cs
@@ -9,6 +9,7 @@ namespace WreckTheBrick
     {
         public Sprite sprite => _sprite;
         public int maxHP => _maxHP;
+        public bool inmune => _maxHP < 0;
 
         [SerializeField]
         private Sprite _sprite;
